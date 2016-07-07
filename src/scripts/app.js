4ae446a70034ee8e049f491374a6b0e40424f0e8
Nathaniel Flores <nathanielflores@Nathaniels-MacBook-Pro.local>
2016-07-07 09:31:06 -0500

    turn in hw 070716

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,49 @@
 import React from 'react'
-import ReactDOM from 'react-dom'	
+import ReactDOM from 'react-dom'
+
+//var today = new Date();
+//var year = today.getFullYear();
 
 const app = function() {
 
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
-		}
-	})
+	
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
-}
+	const AppView = React.createClass ({
+
+		
+
+		getInitialState: function () {
+			
+			return {
+				time: 1900
+		
+			}
+
+
+		},
+
+		_changeTime: () => {
+			
+
+		},
+
+		render: function () {
+			return (
+
+					<div id = "timeContainer">
+						<h1>Time Machine</h1>
+						<div id = "time">{this.state.time}</div>
+						<button id = "timeChanger" onClick = {this._changeTime}></button>	
+					</div>
+
+				)
+			}
+
+		})
+		
+
+			ReactDOM.render(<AppView />, document.querySelector('.container'))
+
+} 
 
 app()
